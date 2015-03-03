0302723e71021f5ecd301d06b5d588221d1066f0
Frei-Cedric Hackbarth <frei-cedric.hackbart@macio.de>
2015-03-03 15:27:00 +0100

    - admininactiveprojects controller hinzugefügt. (Controller Admin_Projects) - inactiveprojects controller entfernt. (War der falsche Controller) - erster Erfolg mit Menü eintrag im Admin Menü

--- init.rb
+++ init.rb
@@ -6,6 +6,8 @@ Redmine::Plugin.register :inactive_projects_list_plugin do
   version '0.0.1'
   url 'https://github.com/FreiCedricHackbarth/Inactive_Projects_List_Plugin.git'
   author_url 'https://github.com/FreiCedricHackbarth'
+  menu :admin_menu, :AdminInactiveProjectsController, {}, :caption => 'Inactive Projects'
 end
 
 require_dependency 'WorkflowHelper2_helper'
+#require_dependency 'AdminInactiveProjectsController'
