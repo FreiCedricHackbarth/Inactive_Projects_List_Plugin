5a237f978948a8c6f4aedd63c805e68fe632f329
Frei-Cedric Hackbarth <frei-cedric.hackbart@macio.de>
2015-03-03 11:16:31 +0100

    Erster Commit: - Initialisierung Plugin - Testweise Überschreibung von den Views: Admin-Index und Admin-Projects - Demo Helper (WorkflowHelper)

---
+++ init.rb
@@ -0,0 +1,11 @@
+#Encoding: UTF-8
+Redmine::Plugin.register :inactive_projects_list_plugin do
+  name 'Inactive Projects List Plugin'
+  author 'Frei-Cedric Hackbarth'
+  description 'The plugin provides a list of all projects, which are inactiv for a specific timespan.'
+  version '0.0.1'
+  url 'https://github.com/FreiCedricHackbarth/Inactive_Projects_List_Plugin.git'
+  author_url 'https://github.com/FreiCedricHackbarth'
+end
+
+require_dependency 'WorkflowHelper2_helper'
