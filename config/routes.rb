2a86263ee97fa4eabe1944c3d8f68938027b030b
Frei-Cedric Hackbarth <frei-cedric.hackbart@macio.de>
2015-03-05 11:48:56 +0100

    delete show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 
 RedmineApp::Application.routes.draw do
   match 'inactiveprojects/index', :controller => 'inactiveprojects', :action => 'index', :via => [:get, :post]
-  match 'inactiveprojects/show', :controller => 'inactiveprojects', :action => 'show', :via => [:get, :post]
 end
