bcd214e22622073044a14bfa9bd10dd7d016a1bc
Frei-Cedric Hackbarth <frei-cedric.hackbart@macio.de>
2015-03-10 11:09:39 +0100

    set Version to 0.8

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :inactive_projects_list_plugin do
   name 'Inactive Projects List Plugin'
   author 'Frei-Cedric Hackbarth'
   description 'The plugin provides a list of all projects, which are inactiv for a specific timespan.'
-  version '0.0.1'
+  version '0.0.8'
   url 'https://github.com/FreiCedricHackbarth/Inactive_Projects_List_Plugin.git'
   author_url 'https://github.com/FreiCedricHackbarth'
   requires_redmine :version_or_higher => '3.0.0'
