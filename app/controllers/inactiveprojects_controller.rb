43021b697f87830dfa16cea802588240eb9df7cb
Frei-Cedric Hackbarth <frei-cedric.hackbart@macio.de>
2015-03-10 12:03:06 +0100

    refactored inactiveprojects_controller

--- app/controllers/inactiveprojects_controller.rb
+++ app/controllers/inactiveprojects_controller.rb
@@ -2,23 +2,19 @@ class InactiveprojectsController < ApplicationController
   layout 'admin'
     
   def index
-	Rails.logger.info "The function index of InactiveprojectsController was called. Info for Frei"
-	
-	# Get the url parameter inactivFor
+	# Get the url parameter inactivFor, if available
 	if params[:inactivFor]
       begin 
 		@inactivFor = params[:inactivFor].to_i
 	  rescue 
 	    @inactivFor = 0;
-		Rails.logger.info "Exception: inactivFor wird auf 0 gesetzt. #{@inactivFor}"
+		Rails.logger.info "Exception: inactivFor is set to default value: #{@inactivFor}"
 	  end
 	else
 	  @inactivFor = 0;
-	  Rails.logger.info "Kein Url Parameter: inactivFor wird auf 0 gesetzt. #{@inactivFor}"
+	  Rails.logger.info "No Url Parameter available: inactivFor is set to default value: #{@inactivFor}"
     end
-	
-	Rails.logger.info "Inactiv For Variable ist: #{@inactivFor}"
-				
+					
 	# Get all projects
     scope = Project.sorted
     @inactivprojects = scope.to_a
@@ -30,21 +26,20 @@ class InactiveprojectsController < ApplicationController
 		
     events = @activity.events(Date.today - @inactivFor , Date.today + 1)
 	
+	Rails.logger.info "Projects: #{@inactivprojects}"
+	Rails.logger.info "There are #{events.length} elements in the events array."
 	Rails.logger.info "Events: #{events}"
-	Rails.logger.info "Es sind #{events.length} Elemente im Array Events."
-	
-	Rails.logger.info "Es sind #{@inactivprojects.length} Elemente vor dem Event Filter im Array Projekte."
+	Rails.logger.info "There are #{@inactivprojects.length} elements in the inactivprojects array before filter."
 	
 	# Delete the project of each event in the timespan
 	events.each do |item|
-		#Rails.logger.info "Akitivtaet Nr #{item.project_id}"
 		@inactivprojects.delete_if{|obj|obj.id == item.project_id}
 	end
 	
 	# Delete all projects which are updated in the timespan
 	@inactivprojects.delete_if{|obj|obj.updated_on > (Date.today - @inactivFor)}
 	
-	Rails.logger.info "Es sind #{@inactivprojects.length} Elemente nach dem Event Filter im Array Projekte."
+	Rails.logger.info "There are #{@inactivprojects.length} elements in the inactivprojects array after filter."
   end
   
 end
